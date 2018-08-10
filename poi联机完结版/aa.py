91840f5582b4d57bb9c14879d279b1c5546c2c49
Czs <czsdemail@163.com>
2018-08-10 15:37:00 +0800

    change some code

--- poi联机完结版/aa.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-
-img_src = Image.open('./img/map1/PD.gif')
-img_src = img_src.convert('RGBA')
-src_strlist = img_src.load()
-for x in range(1, 500):
-    color = src_strlist[x, 700]
-    print(color)
